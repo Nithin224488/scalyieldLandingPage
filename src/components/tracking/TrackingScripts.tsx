89edddcf2450e8fdd18015abb59b4069a957de9e
Nithin Nandakumar <nithin.nandakumar@7hub.it>
2026-07-02 16:42:16 +0530

    pixel id issue fixed

--- src/components/tracking/TrackingScripts.tsx
+++ src/components/tracking/TrackingScripts.tsx
@@ -1,29 +1,26 @@
-import Script from "next/script";
-
+/**
+ * Tracking scripts are inlined at build time for static hosts (e.g. GitHub Pages).
+ * NEXT_PUBLIC_* vars must be set when running `npm run build` in CI/local.
+ */
 export function TrackingScripts() {
   const ga4Id = process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID;
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
   const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
-  console.log(
-    "TrackingScripts: ga4Id:",
-    ga4Id,
-    "gtmId:",
-    gtmId,
-    "pixelId:",
-    pixelId,
-  );
+
   return (
     <>
       {/* Google Tag Manager */}
       {gtmId && !gtmId.includes("XXXX") && (
         <>
-          <Script id="gtm" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${gtmId}');`}
-          </Script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');`,
+            }}
+          />
           <noscript>
             <iframe
               src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
@@ -38,27 +35,39 @@ export function TrackingScripts() {
       {/* Google Analytics 4 */}
       {ga4Id && !ga4Id.includes("XXXX") && (
         <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${ga4Id}`}
-            strategy="afterInteractive"
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${ga4Id}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
+              gtag('js',new Date());gtag('config','${ga4Id}');`,
+            }}
           />
-          <Script id="ga4" strategy="afterInteractive">
-            {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
-            gtag('js',new Date());gtag('config','${ga4Id}');`}
-          </Script>
         </>
       )}
 
-      {/* Meta Pixel */}
+      {/* Meta Pixel — inlined so static hosts detect it without waiting for hydration */}
       {pixelId && !pixelId.includes("XXXX") && (
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-          document,'script','https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init','${pixelId}');fbq('track','PageView');`}
-        </Script>
+        <>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+              n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+              document,'script','https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init','${pixelId}');fbq('track','PageView');`,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+              alt=""
+            />
+          </noscript>
+        </>
       )}
     </>
   );
